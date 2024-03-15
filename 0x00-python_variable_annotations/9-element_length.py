7ca7ea9cefa58be23f8cef209828dd71c47f7fc4
amusukwa <amusukwa@gmail.com>
2024-03-15 13:55:28 +0000

    9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module defining variables in function."""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    return [(i, len(i)) for i in lst]
