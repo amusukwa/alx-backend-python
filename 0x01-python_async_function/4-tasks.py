a8c83d587f6ac4ee5f78e885a68ddb3dfba198f2
amusukwa <amusukwa@gmail.com>
2024-03-19 07:34:58 +0000

    4-tasks.py 2

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+""" Module for tas_wait_n function """
 import asyncio
 from typing import List
 from random import uniform
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Coroutine that waits for a random delay """
     delay = uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Coroutine that spawns wait_random n times with max_delay """
@@ -17,4 +20,3 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
         result = await wait_random(max_delay)
         results.append(result)
     return sorted(results)
-
