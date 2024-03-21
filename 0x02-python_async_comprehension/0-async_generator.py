d918b9e96ff319e6975cf904f497e345c4762a66
amusukwa <amusukwa@gmail.com>
2024-03-21 17:33:31 +0000

    0-async_generator.py error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,11 @@
 
 import asyncio
 import random
+from typing import AsyncIterator
 
-async def async_generator():
-    """ Asynchronous generator """
+
+async def async_generator() -> AsyncIterator[int]:
+    """ Asynchronous generator that yields random numbers """
     for _ in range(10):
         await asyncio.sleep(1)
         # Yield a random number between 0 and 10
