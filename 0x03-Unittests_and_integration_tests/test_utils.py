33b1e378aa76ab42bd6917ec6c2bbf5985a0f6be
amusukwa <amusukwa@gmail.com>
2024-04-07 05:59:13 -0700

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+""" Module for test-utils"""
 import unittest
+from typing import Mapping, Sequence, Any, Dict
 from parameterized import parameterized
 from utils import access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -9,8 +13,19 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected_result: Any) -> None:
+        """ test for nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map")
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence, expected_error_msg: str) -> None:
+        """ test for nested_map exception"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_error_msg)
 
 if __name__ == "__main__":
     unittest.main()
